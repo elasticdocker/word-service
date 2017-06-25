f496042caec36729817418b06df42c66e5617bc8
Manu <Newuser7>
2017-06-25 11:56:10 -0600

    init commit

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ MAINTAINER manishsingla
 LABEL com.app.description="App server" \
       com.app.version="0.1.0"
 
-ENV PORT=3200 NODE_ENV=development OUT_DIR=/var/out
+ENV PORT=3200 NODE_ENV=development VOLUME_LOG=/var/log
 
 # Update .bashrc
 RUN echo 'export PS1="\[\033[90m\]\u@iris.node\[\033[m\]: \[\033[33;1m\]\W\[\033[m\] $> "' >> /root/.bashrc
@@ -16,13 +16,13 @@ WORKDIR /var/application
 # Test should run within docker container (but tests should not be part of published artifact)
 COPY build build
 COPY node_modules node_modules
-COPY public public
+#COPY public public
 
 # remove nodemon because docker should die when node die (And nodemon won't let it die)
 # And an external reconciliation engine like kubernetes to re-start
 RUN npm install -g nodemon
 
-VOLUME [$OUT_DIR]
+VOLUME [$VOLUME_LOG]
 
 #EXPOSE $PORT
 EXPOSE 3200
--- app/server.ts
+++ app/server.ts
@@ -3,9 +3,12 @@ import * as http from 'http';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
+import * as morgan from 'morgan';
+import * as fs from 'fs';
 
 // PORT may be set using docker or env setting
 const port: number = process.env.PORT || 3020;
+const logDir: string = process.env.VOLUME_LOG || '.';
 
 const app = express();
 const server = http.createServer(app);
@@ -17,12 +20,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(cors());
 
+// Before logger
+app.use('/health', (req, res) => {
+	res.sendStatus(200);
+});
+
+const logStream = fs.createWriteStream(path.join(logDir, 'word-service.log'), {flags: 'a'});
+const logFormat = ':remote-addr - [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms';
+
+app.use(morgan(logFormat, {stream: logStream}));
+
 //Add all routes
 require('./routes').default(app);
-
-app.use('/health', (req, res)=>{
-	res.send(200);
-});
 
 //start server
 server.listen(port, () => {
